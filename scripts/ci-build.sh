3437a54fd0c9e980a04c2bd68d27c9bf5ef4ecc8
Benjamin Brown <benjamin.t.brown@hotmail.com>
2026-03-19 19:30:50 -0700

    update pages

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -e
 
 SCRIPT_DIR="$(dirname "$0")"
-cd "$SCRIPT_DIR/../src" && make clean && make js
-cd "../" && mkdir -p dist && cp -rv web/* dist/
+cd "$SCRIPT_DIR/../src"
+make clean
+make js
+cd "$SCRIPT_DIR/.."
+mkdir -p dist
+cp -rv web/* dist/
